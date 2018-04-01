1076086f0c9e02382b0d66e75d83e414ace32e8b
David Slusky <dave@slusky.org>
2018-03-31 22:16:26 -0400

    Draw main UI and handle expose events.

--- src/sysmon.c
+++ src/sysmon.c
@@ -1,8 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <X11/Xlib.h>
+#include <X11/xpm.h>
+#include <X11/extensions/shape.h>
+
+#include "sysmon.h"
+#include "wmgeneral.h"
 
 #include "sysmon-master.xpm"
 #include "sysmon-mask.xbm"
 
-int main(void) {
+void createWindow(int argc, char *argv[]);
+void refreshDisplay(void);
+
+void createWindow(int argc, char *argv[]) {
+    openXwindow(argc, argv,
+        sysmon_master_xpm, sysmon_mask_bits, WIN_WIDTH, WIN_HEIGHT);
+    setMaskXY(0, 0);
+}
+
+void refreshDisplay(void) {
+    copyXPMArea(CPU_SRC_X, CPU_SRC_Y, CPU_WIDTH, CPU_HEIGHT, CPU_DST_X, CPU_DST_Y);
+    copyXPMArea(MEM_SRC_X, MEM_SRC_Y, MEM_WIDTH, MEM_HEIGHT, MEM_DST_X, MEM_DST_Y);
+    copyXPMArea(IO_SRC_X, IO_SRC_Y, IO_WIDTH, IO_HEIGHT, IO_DST_X, IO_DST_Y);
+
+    copyXPMArea(METER_BG_X, METER_BG_Y, METER_WIDTH, METER_HEIGHT, CPU_DST_X+CPU_WIDTH, CPU_DST_Y);
+    copyXPMArea(METER_BG_X, METER_BG_Y, METER_WIDTH, METER_HEIGHT, MEM_DST_X+MEM_WIDTH, MEM_DST_Y);
+    copyXPMArea(METER_BG_X, METER_BG_Y, METER_WIDTH, METER_HEIGHT, IO_DST_X+IO_WIDTH, IO_DST_Y);
+
+    copyXPMArea(SPACER_SRC_X, SPACER_SRC_Y, SPACER_WIDTH, SPACER_HEIGHT, SPACER_DST_X, SPACER_DST_Y);
+
+    RedrawWindowXY(0, 0);
+}
+
+int main(int argc, char *argv[]) {
+    XEvent Event;
+    createWindow(argc, argv);
+    while (1) {
+        refreshDisplay();
+        while (XPending(display)) {
+            XNextEvent(display, &Event);
+            switch (Event.type) {
+                case Expose:
+                    RedrawWindowXY(0, 0);
+                    break;
+                case DestroyNotify:
+                    XCloseDisplay(display);
+                    exit(0);
+                    break;
+                default:
+                    // printf("unhandled event: %d\n", Event.type);
+                    break;
+            }
+        }
+        usleep(250000L);
+    }
     return 0;
 }
---
+++ src/sysmon.h
@@ -0,0 +1,42 @@
+#ifndef __SYSMON_H__
+#define __SYSMON_H__
+
+#define WIN_WIDTH  64
+#define WIN_HEIGHT 64
+
+#define CPU_SRC_X  0
+#define CPU_SRC_Y  73
+#define CPU_DST_X  6
+#define CPU_DST_Y  6
+#define CPU_WIDTH  19
+#define CPU_HEIGHT 9
+
+#define MEM_SRC_X  18
+#define MEM_SRC_Y  73
+#define MEM_DST_X  6
+#define MEM_DST_Y  16
+#define MEM_WIDTH  19
+#define MEM_HEIGHT 9
+
+#define IO_SRC_X   36
+#define IO_SRC_Y   73
+#define IO_DST_X   6
+#define IO_DST_Y   26
+#define IO_WIDTH   19
+#define IO_HEIGHT  9
+
+#define METER_BG_X   31
+#define METER_BG_Y   81
+#define METER_FG_X   0
+#define METER_FG_Y   81
+#define METER_WIDTH  32
+#define METER_HEIGHT 9
+
+#define SPACER_SRC_X  0
+#define SPACER_SRC_Y  90
+#define SPACER_DST_X  5
+#define SPACER_DST_Y  37
+#define SPACER_WIDTH  54
+#define SPACER_HEIGHT 1
+
+#endif // __SYSMON_H__
