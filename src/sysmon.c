bc9bb5bbcf4ff8876bb931221ae9e22436dc518a
David Slusky <dave@slusky.org>
2018-04-01 09:29:50 -0400

    Added functions to draw status meters.

--- src/sysmon.c
+++ src/sysmon.c
@@ -1,4 +1,23 @@
+/*
+    Sysmon.app - system monitoring dockapp for WindowMaker
+    Copyright (C) 2018  David Slusky
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -12,8 +31,18 @@
 #include "sysmon-master.xpm"
 #include "sysmon-mask.xbm"
 
+unsigned int stats[3];
+
 void createWindow(int argc, char *argv[]);
 void refreshDisplay(void);
+void drawMeter(int x, int y, int amount);
+
+
+/* ========================================================================
+ = CREATE_WINDOW
+ =
+ = Create main dockapp window
+ ======================================================================= */
 
 void createWindow(int argc, char *argv[]) {
     openXwindow(argc, argv,
@@ -21,30 +50,75 @@ void createWindow(int argc, char *argv[]) {
     setMaskXY(0, 0);
 }
 
+
+/* ========================================================================
+ = REFRESH_DISPLAY
+ =
+ = Redraw the whole display area
+ ======================================================================= */
+
 void refreshDisplay(void) {
     copyXPMArea(CPU_SRC_X, CPU_SRC_Y, CPU_WIDTH, CPU_HEIGHT, CPU_DST_X, CPU_DST_Y);
     copyXPMArea(MEM_SRC_X, MEM_SRC_Y, MEM_WIDTH, MEM_HEIGHT, MEM_DST_X, MEM_DST_Y);
     copyXPMArea(IO_SRC_X, IO_SRC_Y, IO_WIDTH, IO_HEIGHT, IO_DST_X, IO_DST_Y);
 
-    copyXPMArea(METER_BG_X, METER_BG_Y, METER_WIDTH, METER_HEIGHT, CPU_DST_X+CPU_WIDTH, CPU_DST_Y);
-    copyXPMArea(METER_BG_X, METER_BG_Y, METER_WIDTH, METER_HEIGHT, MEM_DST_X+MEM_WIDTH, MEM_DST_Y);
-    copyXPMArea(METER_BG_X, METER_BG_Y, METER_WIDTH, METER_HEIGHT, IO_DST_X+IO_WIDTH, IO_DST_Y);
+    copyXPMArea(METER_BG_X, METER_BG_Y, METER_WIDTH, METER_HEIGHT, CPU_DST_X+CPU_WIDTH+3, CPU_DST_Y);
+    copyXPMArea(METER_BG_X, METER_BG_Y, METER_WIDTH, METER_HEIGHT, MEM_DST_X+MEM_WIDTH+3, MEM_DST_Y);
+    copyXPMArea(METER_BG_X, METER_BG_Y, METER_WIDTH, METER_HEIGHT, IO_DST_X+IO_WIDTH+3, IO_DST_Y);
 
     copyXPMArea(SPACER_SRC_X, SPACER_SRC_Y, SPACER_WIDTH, SPACER_HEIGHT, SPACER_DST_X, SPACER_DST_Y);
-
-    RedrawWindowXY(0, 0);
+    RedrawWindow();
 }
+
+
+/* ========================================================================
+ = DRAW_METER
+ =
+ = Display meter at XY coordinates
+ ======================================================================= */
+
+void drawMeter(int x, int y, int amount) {
+    amount = CLAMP(amount, 0, 100);
+    copyXPMArea(METER_BG_X, METER_BG_Y, METER_WIDTH, METER_HEIGHT, x, y);
+    copyXPMArea(METER_FG_X, METER_FG_Y, amount*METER_WIDTH/100, METER_HEIGHT, x, y);
+    RedrawRegion(x, y, METER_WIDTH, METER_HEIGHT);
+}
+
+
+/* ========================================================================
+ = UPDATE_STATS
+ =
+ = Gather system stats and update display
+ ======================================================================= */
+
+void updateStats() {
+    drawMeter(CPU_METER_X, CPU_METER_Y, stats[STATS_CPU]);
+    drawMeter(MEM_METER_X, MEM_METER_Y, stats[STATS_MEM]);
+    drawMeter(IO_METER_X, IO_METER_Y, stats[STATS_IO]);
+}
+
+
+/* ========================================================================
+ = MAIN
+ =
+ = You have entered a maze of twisty passages, all alike
+ ======================================================================= */
 
 int main(int argc, char *argv[]) {
     XEvent Event;
+
+    memset(stats, 0, sizeof(stats));
+
     createWindow(argc, argv);
+    refreshDisplay();
+
     while (1) {
-        refreshDisplay();
+        updateStats();
         while (XPending(display)) {
             XNextEvent(display, &Event);
             switch (Event.type) {
                 case Expose:
-                    RedrawWindowXY(0, 0);
+                    refreshDisplay();
                     break;
                 case DestroyNotify:
                     XCloseDisplay(display);
--- src/sysmon.h
+++ src/sysmon.h
@@ -1,36 +1,66 @@
+/*
+    Sysmon.app - system monitoring dockapp for WindowMaker
+    Copyright (C) 2018  David Slusky
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef __SYSMON_H__
 #define __SYSMON_H__
+
+enum {
+    STATS_CPU,
+    STATS_MEM,
+    STATS_IO
+};
 
 #define WIN_WIDTH  64
 #define WIN_HEIGHT 64
 
-#define CPU_SRC_X  0
-#define CPU_SRC_Y  73
-#define CPU_DST_X  6
-#define CPU_DST_Y  6
-#define CPU_WIDTH  19
-#define CPU_HEIGHT 9
+#define CPU_SRC_X   1
+#define CPU_SRC_Y   74
+#define CPU_DST_X   7
+#define CPU_DST_Y   7
+#define CPU_METER_X 27
+#define CPU_METER_Y 7
+#define CPU_WIDTH   17
+#define CPU_HEIGHT  7
 
-#define MEM_SRC_X  18
-#define MEM_SRC_Y  73
-#define MEM_DST_X  6
-#define MEM_DST_Y  16
-#define MEM_WIDTH  19
-#define MEM_HEIGHT 9
+#define MEM_SRC_X   19
+#define MEM_SRC_Y   74
+#define MEM_DST_X   7
+#define MEM_DST_Y   17
+#define MEM_METER_X 27
+#define MEM_METER_Y 17
+#define MEM_WIDTH   17
+#define MEM_HEIGHT  7
 
-#define IO_SRC_X   36
-#define IO_SRC_Y   73
-#define IO_DST_X   6
-#define IO_DST_Y   26
-#define IO_WIDTH   19
-#define IO_HEIGHT  9
+#define IO_SRC_X   37
+#define IO_SRC_Y   74
+#define IO_DST_X   7
+#define IO_DST_Y   27
+#define IO_METER_X 27
+#define IO_METER_Y 27
+#define IO_WIDTH   17
+#define IO_HEIGHT  7
 
-#define METER_BG_X   31
-#define METER_BG_Y   81
-#define METER_FG_X   0
-#define METER_FG_Y   81
-#define METER_WIDTH  32
-#define METER_HEIGHT 9
+#define METER_BG_X   32
+#define METER_BG_Y   82
+#define METER_FG_X   1
+#define METER_FG_Y   82
+#define METER_WIDTH  30
+#define METER_HEIGHT 7
 
 #define SPACER_SRC_X  0
 #define SPACER_SRC_Y  90
@@ -38,5 +68,11 @@
 #define SPACER_DST_Y  37
 #define SPACER_WIDTH  54
 #define SPACER_HEIGHT 1
+
+#define MIN(a, b)  (((a) < (b)) ? (a) : (b))
+#define MAX(a, b)  (((a) > (b)) ? (a) : (b))
+#define ABS(a)	   (((a) < 0) ? -(a) : (a))
+
+#define CLAMP(x, low, high)  (((x) > (high)) ? (high) : (((x) < (low)) ? (low) : (x)))
 
 #endif // __SYSMON_H__
